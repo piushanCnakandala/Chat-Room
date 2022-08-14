bc71339ee6925a00abc29df1ed3a59aae97b5adb
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-08-14 18:25:09 +0530

    LoginForm ui created

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane loginformPane;
+    public JFXTextField txtUserName;
+    public JFXButton btnLogin;
+}
